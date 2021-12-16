950847cbaf86e85d2d069299588dd1778b13b1ac
yaya-nyan <Florian.Denker@live.de>
2021-12-16 08:30:26 +0100

    added remaining chat colors to ColorUtil

--- src/main/java/yaya/miningbuddies/Utilities/ColorUtil.java
+++ src/main/java/yaya/miningbuddies/Utilities/ColorUtil.java
@@ -2,7 +2,21 @@ package yaya.miningbuddies.Utilities;
 
 public class ColorUtil
 {
-	public static final int DARK_RED = getColorFromRGBA(145, 42, 42, 255);
+	public static final int WHITE = getColorFromRGBA(255, 255, 255, 255);
+	public static final int GRAY = getColorFromRGBA(170, 170, 170, 255);
+	public static final int DARK_GRAY = getColorFromRGBA(85, 85, 85, 255);
+	public static final int BLACK = getColorFromRGBA(0, 0, 0, 255);
+	public static final int YELLOW = getColorFromRGBA(255, 255, 85, 255);
+	public static final int GREEN = getColorFromRGBA(85, 255, 85, 255);
+	public static final int DARK_GREEN = getColorFromRGBA(0, 170, 0, 255);
+	public static final int AQUA = getColorFromRGBA(85, 255, 255, 255);
+	public static final int DARK_AQUA = getColorFromRGBA(0, 170, 170, 255);
+	public static final int BLUE = getColorFromRGBA(85, 85, 255, 255);
+	public static final int DARK_BLUE = getColorFromRGBA(0, 0, 170, 255);
+	public static final int LIGHT_PURPLE = getColorFromRGBA(255, 85, 255, 255);
+	public static final int DARK_PURPLE = getColorFromRGBA(170, 0, 170, 255);
+	public static final int RED = getColorFromRGBA(255, 85, 85, 255);
+	public static final int DARK_RED = getColorFromRGBA(170, 0, 0, 255);
 	public static final int GOLD = getColorFromRGBA(255, 170, 0, 255);
 	
 	public static int getColorFromRGBA(int r, int g, int b, int a)
